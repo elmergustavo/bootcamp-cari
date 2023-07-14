8b5eecd09c8ed90b93009f5908c8cc7b999648db
Gustavo Pu <gpu@cari.net>
2023-07-14 02:07:16 -0600

    install livewire and alpineJS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./resources/view/**/*.blade.php",
+      "./resources/**/*.js",
+      './resources/**/*.blade.php',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
